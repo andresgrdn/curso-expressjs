7e59590dfc9a3f6605ff7c58f306b5571d1ea64c
Andres Gordon <gordon_andres@yahoo.es>
2025-06-27 11:44:03 -0500

    [add] método post para recibir json
    body parser se usa para poder hacer parse de los datos

--- app.js
+++ app.js
@@ -29,10 +29,36 @@ app.get("/search", (req, res) => {
     const category = req.query.categoria || "Todas";
 
     res.send(`
-            <h2>Resultado de busqueda:</h2>
+            <h2>Resultado de búsqueda:</h2>
             <p>Termino: ${terms}</p>
             <p>Categoría: ${category}</p>
         `);
+});
+
+app.post("/form", (req, res) => {
+    const name = req.body.nombre || "Anó nimo";
+    const email = req.body.email || "No proporcionade";
+
+    res.json({
+        message: "datos recibidos",
+        data: {
+            name,
+            email,
+        },
+    });
+});
+
+app.post("/api/data", (req, res) => {
+    const data = req.body;
+
+    if (!data || Object.keys(data).length === 0) {
+        return res.status(400).json({ error: "No tengo eso perro" });
+    }
+
+    res.status(201).json({
+        message: "Datos JSON recibidos",
+        data,
+    });
 });
 
 
