f0ff046bff11dfb7e6d60d2a5b35773f2e1aa0f1
Andres Gordon <gordon_andres@yahoo.es>
2025-06-27 11:33:44 -0500

    [add] body-parser, rutas dinámicas y parámetros

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +16,25 @@ app.get("/", (req, res) => {
             <p>Nueva perra en la ciudad aaaaaaaaaaaaaaaaaaaa................222</p>
         `);
 });
+
+//  ruta dinámica
+app.get("/users/:id", (req, res) => {
+    const userId = req.params.id;
+    res.send(`Mostrar info del usuario con id: ${userId}`);
+});
+
+// parámetros query
+app.get("/search", (req, res) => {
+    const terms = req.query.termino || "No especificado";
+    const category = req.query.categoria || "Todas";
+
+    res.send(`
+            <h2>Resultado de busqueda:</h2>
+            <p>Termino: ${terms}</p>
+            <p>Categoría: ${category}</p>
+        `);
+});
+
 
 app.listen(PORT, () => {
     console.log(`App corriendose en el puerto ${PORT}!`);
